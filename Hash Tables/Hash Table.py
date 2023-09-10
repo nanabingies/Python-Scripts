853f339a8e13e1014b921e2aec2c71933d18d1d2
Nana Bingies <nanabingies1@gmail.com>
2023-09-10 01:34:01 +0000

    Update Hash Table.py

--- Hash Tables/Hash Table.py
+++ Hash Tables/Hash Table.py
@@ -1,1 +1,20 @@
 # Hash Table implementation
+
+class HashTable:
+    def __init__(self) -> None:
+        self.MAX = 100
+        self.arr = [None for i in range(self.MAX)]
+
+    def get_hash(self, key):
+        h = 0
+        for char in key:
+            h += ord(char)
+        return h % self.MAX
+    
+    def add(self, key, val):
+        h = self.get_hash(key)
+        self.arr[h] = val
+    
+if __name__ == "__main__":
+    hash = HashTable()
+    hash.add('March 6', 130)
