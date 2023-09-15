a90171416587659e7617c77034d56b349ee5e10a
Nana Bingies <nanabingies1@gmail.com>
2023-09-15 23:14:03 +0000

    Update Test.py

--- Graph Theory/Directed Graphs/Test.py
+++ Graph Theory/Directed Graphs/Test.py
@@ -9,3 +9,10 @@ nx.draw_networkx_nodes(G, pos, node_size=500)
 nx.draw_networkx_edges(G, pos, edgelist=G.edges(), edge_color='black')
 nx.draw_networkx_labels(G, pos)
 plt.show()
+
+G = nx.cycle_graph(4, create_using=nx.DiGraph())
+nx.add_cycle(G, [10, 11, 12])
+[
+    len(c)
+    for c in sorted(nx.strongly_connected_components(G), key=len, reverse=True)
+]
