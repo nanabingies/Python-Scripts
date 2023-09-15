7a169f74604c33a325bf536ea5c67b4477f91a27
Nana Bingies <nanabingies1@gmail.com>
2023-09-15 23:10:41 +0000

    Update Test.py

--- Graph Theory/Directed Graphs/Test.py
+++ Graph Theory/Directed Graphs/Test.py
@@ -1,3 +1,11 @@
 import networkx as nx 
 import matplotlib.pyplot as plt
 
+G = nx.DiGraph()
+G.add_edges_from([('A', 'B'), ('A', 'C'), ('C', 'B')])
+
+pos = nx.spring_layout(G)
+nx.draw_networkx_nodes(G, pos, node_size=500)
+nx.draw_networkx_edges(G, pos, edgelist=G.edges(), edge_color='black')
+nx.draw_networkx_labels(G, pos)
+plt.show()
