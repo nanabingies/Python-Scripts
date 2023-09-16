975616e47e1b31fecfc57c082611d3eba94ac17c
Nana Bingies <nanabingies1@gmail.com>
2023-09-16 02:05:15 +0000

    Update BFS.py

--- Graph Theory/BFS.py
+++ Graph Theory/BFS.py
@@ -1,3 +1,15 @@
+# BFS pseudocode
+
+"""
+create a queue Q 
+mark v as visited and put v into Q 
+while Q is non-empty 
+    remove the head u of Q 
+    mark and enqueue all (unvisited) neighbors of u
+"""
+
+from typing import TypeVar
+T = TypeVar("T")
 
 graph = {
   '5' : ['3','7'],
@@ -7,3 +19,22 @@ graph = {
   '4' : ['8'],
   '8' : []
 }
+
+
+def BFS(visited: list, graph: dict, startNode: T):
+    queue: list = []
+    queue.append(startNode)
+    visited.append(startNode)
+    
+    while queue:
+        head: T = queue.pop(0)
+        print(head, end=' ')
+
+        for neighbor in graph[head]:
+            if neighbor not in visited:
+                visited.append(neighbor)
+                queue.append(neighbor)
+
+if __name__ == "__main__":
+    visited: list = []
+    BFS(visited, graph, '5')
