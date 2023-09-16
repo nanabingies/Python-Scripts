f288cb8d5d068684126b7d7c37d0ec2bf92a3f9e
Nana Bingies <nanabingies1@gmail.com>
2023-09-16 01:48:38 +0000

    Update DFS.py

--- Graph Theory/DFS.py
+++ Graph Theory/DFS.py
@@ -1,5 +1,8 @@
 # Using a Python dictionary to act as an adjacency list
 
+from typing import TypeVar
+T = TypeVar("T")
+
 graph = {
   '5' : ['3','7'],
   '3' : ['2', '4'],
@@ -8,3 +11,16 @@ graph = {
   '4' : ['8'],
   '8' : []
 }
+
+
+def DFS(visited: set, graph:dict, node: T):
+    for node in graph:
+        if node not in visited:
+            print(node)
+            visited.add(node)
+            for neighbor in graph[node]:
+                DFS(visited, graph, neighbor)
+
+if __name__ == "__main__":
+    visited = set()
+    DFS(visited, graph, '5')
